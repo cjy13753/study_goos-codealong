32d93cb91e78d398a76df284fd6e86ca5f3b303f
Jun Choi <cjy13753@gmail.com>
2021-08-21 14:55:57 +0900

    First end-to-end test - Red(stub helper classes only).

---
+++ src/test/java/endtoend/auctionsniper/ApplicationRunner.java
@@ -0,0 +1,12 @@
+package endtoend.auctionsniper;
+
+public class ApplicationRunner {
+    public void startBiddingIn(FakeAuctionServer auctionServer) {
+    }
+
+    public void showsSniperHasLostAuction() {
+    }
+
+    public void stop() {
+    }
+}
---
+++ src/test/java/endtoend/auctionsniper/FakeAuctionServer.java
@@ -0,0 +1,21 @@
+package endtoend.auctionsniper;
+
+public class FakeAuctionServer {
+
+    public FakeAuctionServer(String itemId) {
+    }
+
+    public void startSellingItem() {
+    }
+
+    public void hasReceivedJoinRequestFromSniper() {
+    }
+
+    public void announceClosed() {
+    }
+
+    public void stop() {
+    }
+
+
+}
