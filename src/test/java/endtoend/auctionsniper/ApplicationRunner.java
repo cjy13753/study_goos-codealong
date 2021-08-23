492c370cfc88cc2744f21af3ed7815c16b8f78c2
Jun Choi <cjy13753@gmail.com>
2021-08-23 18:38:53 +0900

    First end-to-end test - Red (implemented ApplicationRunner and FakeAuctionServer, the test is failing as in the book, page 96 - "all top level windows contained 0 JFrame (with name "Auction Sniper Main" and showing on screen)").
    1. Upgrade gradle version to 3.0 for compatibility with IntelliJ Idea 2021.1
    2. The result of the test is slightly different from GOOS in that my application stopped in "startBiddingIn()" whereas, in the book, the app stopped in the @After method". (refer to p.96)

--- src/test/java/endtoend/auctionsniper/ApplicationRunner.java
+++ src/test/java/endtoend/auctionsniper/ApplicationRunner.java
@@ -1,12 +1,40 @@
 package endtoend.auctionsniper;
 
+import auctionsniper.Main;
+
+import static auctionsniper.Main.STATUS_JOINING;
+import static auctionsniper.Main.STATUS_LOST;
+import static endtoend.auctionsniper.FakeAuctionServer.XMPP_HOSTNAME;
+
 public class ApplicationRunner {
-    public void startBiddingIn(FakeAuctionServer auctionServer) {
+    private static final String SNIPER_ID = "sniper";
+    private static final String SNIPER_PASSWORD = "sniper";
+
+    private AuctionSniperDriver driver;
+
+    public void startBiddingIn(FakeAuctionServer auction) {
+        Thread thread = new Thread("Test Application") {
+            @Override public void run() {
+                try {
+                    Main.main(XMPP_HOSTNAME, SNIPER_ID, SNIPER_PASSWORD, auction.getItemId());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.setDaemon(true);
+        thread.start();
+        driver = new AuctionSniperDriver(1000);
+        driver.showsSniperStatus(STATUS_JOINING);
     }
 
     public void showsSniperHasLostAuction() {
+        driver.showsSniperStatus(STATUS_LOST);
     }
 
     public void stop() {
+        if (driver != null) {
+            driver.dispose();
+        }
     }
 }
