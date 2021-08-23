2a993e3b7737805c2996132c8779d36b8b01c4a0
Jun Choi <cjy13753@gmail.com>
2021-08-23 19:45:45 +0900

    First end-to-end test - Red (added sniperStatus Label to the MainWindow, the test is failing as in the book, page 98 - "Expected: is not null, got: null").

--- src/test/java/endtoend/auctionsniper/AuctionSniperDriver.java
+++ src/test/java/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -1,6 +1,7 @@
 package endtoend.auctionsniper;
 
 import auctionsniper.Main;
+import auctionsniper.ui.MainWindow;
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.driver.JFrameDriver;
 import com.objogate.wl.swing.driver.JLabelDriver;
@@ -15,7 +16,7 @@ public class AuctionSniperDriver extends JFrameDriver {
                 new AWTEventQueueProber(timeoutMillis, 100));
     }
     public void showsSniperStatus(String statusText) {
-        new JLabelDriver(this, named(Main.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+        new JLabelDriver(this, named(MainWindow.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
     }
 
 
